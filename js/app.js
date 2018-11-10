a64427a5f56c4f931fa2ef24c65da33e6dfcc8c1
DevlopperPauHa <36643560+DevlopperPauHa@users.noreply.github.com>
2018-11-10 12:19:11 +0100

    made captcha required

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-// Made by Dem0niaX
+// Made by Dem0niaX & Paulé
 
 $('.fadeIn').addClass('animated fadeIn');
+
+$( '#btn-validate' ).click(function(){
+    var $captcha = $( '#recaptcha' ),
+        response = grecaptcha.getResponse();
+    
+    if (response.length === 0) {
+      $( '.msg-error').text( "Merci de remplir le reCAPTCHA." );
+      if( !$captcha.hasClass( "error" ) ){
+        $captcha.addClass( "error" );
+      }
+    } else {
+      $( '.msg-error' ).text('');
+      $captcha.removeClass( "error" );
+      return;
+    }
+})

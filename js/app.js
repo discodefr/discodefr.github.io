c271c7180fcaf12caea7d42b5499d813f384849c
discodefr <discodefr.contact@gmail.com>
2018-11-19 22:18:58 +0100

    Test

--- js/app.js
+++ js/app.js
@@ -1,24 +1,3 @@
 // Made by Dem0niaX & Paulé
 
 $('.fadeIn').addClass('animated fadeIn');
-
-$(document).ready(function(){
-    (function() {
-      function checkRecaptcha() {
-        res = $('#g-recaptcha-response').val();
-
-        if (res == "" || res == undefined || res.length == 0)
-          return false;
-        else
-          return true;
-      }
-
-
-      $('form').submit(function(e) {
-        if(!checkRecaptcha()) {
-            $( '.msg-error').text( "Merci de cocher le reCAPTCHA." )
-          return false;
-        }
-      });
-    }());
-});

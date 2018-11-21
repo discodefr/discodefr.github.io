b5fd8a7c5af25b6ba3c51c317e02c456c4de6e02
discodefr <discodefr.contact@gmail.com>
2018-11-21 18:38:13 +0100

    On teste le redirect

--- js/app.js
+++ js/app.js
@@ -5,3 +5,6 @@ $('.fadeIn').addClass('animated fadeIn');
 function recaptcha_callback() {
   $('#sell_house_submit').removeAttr('disabled');
 }
+
+var submitbtn = document.getElementById("sell_house_submit")
+submitbtn.onclick = window.location.href("contactposted.html")

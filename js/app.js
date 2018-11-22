f53067b059c3a39482b9ca6cf80ce4c12bc9cbc3
discodefr <discodefr.contact@gmail.com>
2018-11-22 19:28:53 +0100

    Test.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,21 @@ $('.fadeIn').addClass('animated fadeIn');
 function recaptcha_callback() {
   $('#submitbtn').removeAttr('disabled');
 }
+
+/* attach a submit handler to the form */
+$("#ctcform").submit(function(event) {
+
+/* stop form from submitting normally */
+event.preventDefault();
+/* get the action attribute from the <form action=""> element */
+var $form = $( this ),
+url = $form.attr( 'action' );
+
+/* Send the data using post with element id name and name2*/
+var posting = $.post( url, { nom: $('#nom').val(), prenom: $('#prenom').val(), discordtag: $("#discordtag").val(), email: $("#email").val(), message: $("#message").val() } );
+
+/* Alerts the results */
+posting.done(function( data ) {
+alert('success');
+});
+});

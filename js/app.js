3fa2d09c03504272ace5798a4acc65bbf3d68ee9
discodefr <discodefr.contact@gmail.com>
2018-11-22 18:43:25 +0100

    Test d'une checkbox

--- js/app.js
+++ js/app.js
@@ -5,8 +5,3 @@ $('.fadeIn').addClass('animated fadeIn');
 function recaptcha_callback() {
   $('#submitbtn').removeAttr('disabled');
 }
-
-function contactredirect() {
-var submitbtn = document.getElementById("submitbtn")
-submitbtn.onclick = window.location.assign('/contactposted.html');
-}

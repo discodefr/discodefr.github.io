001fe576dffd85bc2d7e2d7a95ce402890f9b911
discodefr <discodefr.contact@gmail.com>
2018-11-23 19:14:05 +0100

    Testing ajax...

--- js/app.js
+++ js/app.js
@@ -5,3 +5,22 @@ $('.fadeIn').addClass('animated fadeIn');
 function recaptcha_callback() {
   $('#submitbtn').removeAttr('disabled');
 }
+
+$("#ctcform").submit(function(e) {
+
+
+  var form = $(this);
+  var url = form.attr('action');
+
+  $.ajax({
+         type: "POST",
+         url: url,
+         data: form.serialize(),
+         success: function(data)
+         {
+             alert(data);
+         }
+       });
+
+  e.preventDefault();
+});

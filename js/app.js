51ba5ef2115c80f3cad9d1e3048f0a1fe81320eb
discodefr <discodefr.contact@gmail.com>
2018-11-26 22:23:07 +0100

    Test ?

--- js/app.js
+++ js/app.js
@@ -5,3 +5,17 @@ $('.fadeIn').addClass('animated fadeIn');
 function recaptcha_callback() {
   $('#submitbtn').removeAttr('disabled');
 }
+
+function readMore() {
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readMoreBtn");
+
+  if (!moreText.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    btnText.innerHTML = "Moins..."; 
+    moreText.style.display = "inline";
+  }
+}

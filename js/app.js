ff85caa30abf01891c2dcc270c343944e9d89248
discodefr <discodefr.contact@gmail.com>
2018-11-27 20:16:39 +0100

    Testtt !!

--- js/app.js
+++ js/app.js
@@ -5,17 +5,3 @@ $('.fadeIn').addClass('animated fadeIn');
 function recaptcha_callback() {
   $('#submitbtn').removeAttr('disabled');
 }
-
-$(function () {
-  $("div").slice(0, 3).show();
-  $("#loadMore").on('click', function (e) {
-      e.preventDefault();
-      $("div:hidden").slice(0, 3).slideDown();
-      if ($("div:hidden").length == 0) {
-          $("#load").fadeOut('slow');
-      }
-      $('html,body').animate({
-          scrollTop: $(this).offset().top
-      }, 1500);
-  });
-});

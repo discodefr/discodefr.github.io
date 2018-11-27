2db48446efcbf9e4e9e71e15ded5bcddf963a236
discodefr <discodefr.contact@gmail.com>
2018-11-27 21:15:46 +0100

    jQuery test

--- js/app.js
+++ js/app.js
@@ -5,3 +5,17 @@ $('.fadeIn').addClass('animated fadeIn');
 function recaptcha_callback() {
   $('#submitbtn').removeAttr('disabled');
 }
+
+$(function () {
+    $("div").slice(0, 4).show();
+    $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $("div:hidden").slice(0, 4).slideDown();
+        if ($("div:hidden").length == 0) {
+            $("#load").fadeOut('slow');
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().top
+        }, 1500);
+    });
+});

4df1683d7e8af555a1f87ebd03d0aa2581a07ce0
discodefr <discodefr.contact@gmail.com>
2018-11-27 19:52:10 +0100

    Eeeet beh

--- js/app.js
+++ js/app.js
@@ -19,18 +19,3 @@ $(function () {
       }, 1500);
   });
 });
-
-$('a[href=#top]').click(function () {
-  $('body,html').animate({
-      scrollTop: 0
-  }, 600);
-  return false;
-});
-
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 50) {
-      $('.totop a').fadeIn();
-  } else {
-      $('.totop a').fadeOut();
-  }
-});

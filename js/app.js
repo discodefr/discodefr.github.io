df571c10cc913abc81dc6d3c7e6fcb3eea89a959
discodefr <discodefr.contact@gmail.com>
2018-11-27 18:54:05 +0100

    Nice !

--- js/app.js
+++ js/app.js
@@ -6,13 +6,31 @@ function recaptcha_callback() {
   $('#submitbtn').removeAttr('disabled');
 }
 
-$(function(){ /* to make sure the script runs after page load */
-
-    $('a.read_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
-
-        event.preventDefault(); /* prevent the a from changing the url */
-        $(this).parents('.item').find('.more_text').show(); /* show the .more_text span */
-
+$(function () {
+    $("div").slice(0, 4).show();
+    $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $("div:hidden").slice(0, 4).slideDown();
+        if ($("div:hidden").length == 0) {
+            $("#load").fadeOut('slow');
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().top
+        }, 1500);
     });
+});
 
+$('a[href=#top]').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.totop a').fadeIn();
+    } else {
+        $('.totop a').fadeOut();
+    }
 });

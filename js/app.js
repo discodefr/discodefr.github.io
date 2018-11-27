25b09da3e09f9402b944f2bc7588753c4a5fbde7
discodefr <discodefr.contact@gmail.com>
2018-11-27 19:24:37 +0100

    Test

--- js/app.js
+++ js/app.js
@@ -5,3 +5,32 @@ $('.fadeIn').addClass('animated fadeIn');
 function recaptcha_callback() {
   $('#submitbtn').removeAttr('disabled');
 }
+
+$(function () {
+  $("div").slice(0, 3).show();
+  $("#loadMore").on('click', function (e) {
+      e.preventDefault();
+      $("div:hidden").slice(0, 3).slideDown();
+      if ($("div:hidden").length == 0) {
+          $("#load").fadeOut('slow');
+      }
+      $('html,body').animate({
+          scrollTop: $(this).offset().top
+      }, 1500);
+  });
+});
+
+$('a[href=#top]').click(function () {
+  $('body,html').animate({
+      scrollTop: 0
+  }, 600);
+  return false;
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+      $('.totop a').fadeIn();
+  } else {
+      $('.totop a').fadeOut();
+  }
+});

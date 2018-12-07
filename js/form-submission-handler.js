2f6a42565118ab2d78e623435baf217f8bd2155d
discodefr <discodefr.contact@gmail.com>
2018-12-07 19:17:55 +0100

    …

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -1,4 +1,4 @@
-//(function() {
+(function() {
 //  function validEmail(email) {
 //    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 //    return re.test(email);
